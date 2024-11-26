3f35bdfe8d2a51f840fc550f62bbfad6b3bf641a
diegofmo0802 <diegofmo0802@gmail.com>
2024-11-26 22:41:20 +0000

    added querys

--- API REST/src/query.js
+++ API REST/src/query.js
@@ -1,5 +1,14 @@
 import connection from "./config/connection.js";
 
+/**
+ * @typedef {import('./types.d.ts').student.data} student
+ * @typedef {import('./types.d.ts').student.update} studentUpdate
+ */
+
+/**
+ * @type {string}
+ */
+
 const STUDENTS_TABLE = 'students';
 
 /**
@@ -26,4 +35,35 @@ export async function getStudents(page, limit) {
     const skip = (page - 1) * limit;
     console.log("getting students...");
     return await query(`SELECT * FROM ${STUDENTS_TABLE} WHERE 1 LIMIT ? OFFSET ?`, [limit, skip]);
+}
+/**
+ * insert a new student in the database
+ * @param {student} data the data for the student
+ */
+export async function insertStudent(data) {
+    const { first_name, last_name, code, note1, note2, note3 } = data;
+    console.log("inserting student...");
+    return await query(`INSERT INTO ${STUDENTS_TABLE} (code, first_name, last_name, note1, note2, note3) VALUES (?, ?, ?, ?, ?, ?)`, [first_name, last_name, code, note1, note2, note3]);
+}
+/**
+ * update a student in the database
+ * @param {student} data the data of student
+ * @param {string} code the code of student to update
+ */
+export async function updateStudent(code, data) {
+    const { first_name, last_name, note1, note2, note3 } = data;
+    console.log("updating student...");
+    return await query(
+        `UPDATE ${STUDENTS_TABLE} SET first_name = ?, last_name = ?, note1 = ?, note2 = ?, note3 = ? WHERE code = ?`,
+        [first_name, last_name, note1, note2, note3, code]
+    );
+}
+
+/**
+ * delete a student in the database
+ * @param {string} code the code of student to delete
+ */
+export async function deleteStudent(code) {
+    console.log("deleting student...");
+    return await query(`DELETE FROM ${STUDENTS_TABLE} WHERE code = ?`, [code]);
 }
