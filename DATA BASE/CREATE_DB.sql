eb6b75ff100d6a91e5ee6845eee80737d28081d2
diegofmo0802 <diegofmo0802@gmail.com>
2024-11-27 22:25:33 +0000

    DB moved to postgres to pass the requirements

--- DATA BASE/CREATE_DB.sql
+++ DATA BASE/CREATE_DB.sql
@@ -1,12 +1,34 @@
-CREATE DATABASE pf-db;
+START TRANSACTION;
 
-USE pf-db;
+-- Set timezone to UTC
+SET time_zone = "+00:00";
 
-CREATE TABLE students (
-  code INT PRIMARY KEY NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  note1 FLOAT,
-  note2 FLOAT,
-  note3 FLOAT
-);
+-- Create table student if not exists
+CREATE TABLE IF NOT EXISTS `students` (
+  `code` int NOT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `note1` float DEFAULT '0',
+  `note2` float DEFAULT '0',
+  `note3` float DEFAULT '0',
+  `average` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Create trigger to calculate average before insert or update 
+DELIMITER $$
+CREATE TRIGGER `calculate_average` BEFORE INSERT ON `students` FOR EACH ROW BEGIN
+  SET NEW.average = (NEW.note1 + NEW.note2 + NEW.note3) / 3;
+END
+$$
+DELIMITER ;
+
+-- Create trigger to update average after update
+DELIMITER $$
+CREATE TRIGGER `update_average` BEFORE UPDATE ON `students` FOR EACH ROW BEGIN
+  SET NEW.average = (NEW.note1 + NEW.note2 + NEW.note3) / 3;
+END
+$$
+DELIMITER ;
+
+COMMIT;
