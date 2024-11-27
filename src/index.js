f050dc55c43a33ab80e7e67dd6975cd496a1cbbd
diegofmo0802 <diegofmo0802@gmail.com>
2024-11-27 00:45:38 +0000

    added client rules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import './config/env.js';
+import path from 'path';
 import express from 'express';
 import { addRoutes } from './apiRoutes.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
 addRoutes(app);
 
-app.listen(3000);
+app.use('/client', express.static(path.join(__dirname, '../client')));
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '../client', 'index.html')); });
+
+app.listen(3000, () => {
+    console.log(`Server running`);
+});
